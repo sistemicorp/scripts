ead62a1b882fea9bea358d3b2c83e2cf1276ce68
sistemicorp <martin.guthrie@sistemi.ca>
2019-07-18 15:00:26 -0400

    Use ampy module for pyboard stuff
    remove my old versions of things, that is, upyboard,py uses ampy api

--- public/prism/drivers/micropythonbrd/upybrd.py
+++ public/prism/drivers/micropythonbrd/upybrd.py
@@ -7,26 +7,25 @@ Martin Guthrie
 """
 try:
     # for when used by Prism
-    import public.prism.drivers.micropythonbrd.pyboard as pyboard
     from public.prism.drivers.micropythonbrd.list_serial import serial_ports
 except:
     # for when called by main
-    import pyboard
     from list_serial import serial_ports
 
 import sys
-import serial
+import os
 import time
 import logging
-import json
 import argparse
 
+import ampy.files as files
+import ampy.pyboard as pyboard
 
 VERSION = "0.0.1"
 
 
 class MicroPyBrd(object):
-    """  Driver for the micopython baord
+    """  Driver for the micopython board
 
     http://docs.micropython.org/en/v1.9.4/pyboard/pyboard/quickref.html
 
@@ -56,7 +55,7 @@ class MicroPyBrd(object):
         Use the Command Line Interface (CLI),
          python upybrd.py --port COM3 --set-id 1
 
-        or, programitically,
+        or, programmatically,
          pyb = MicroPyBrd(logging)
          pyb.set_pyb_id(<port>, <id>)
          pyb.close()
@@ -95,33 +94,19 @@ class MicroPyBrd(object):
     def __init__(self, loggerIn=None):
         if loggerIn: self.logger = loggerIn
         else: self.logger = self.StubLogger()
-        self.pyb = None
-        self.channel = None
-        self.serial = None
+        self.pyb = None       # handle to the pyboard
+        self.serial = None    # serial port to the pyboard
         self.logger.debug("Done")
 
     def version(self):
         return VERSION
 
-    def _enter_raw_repl(self):
-        # A port/hack of pyboard to test a port for micropython
-        self.serial.write(b'\r\x03\x03') # ctrl-C twice: interrupt any running program
-        # flush input (without relying on serial.flushInput())
-        n = self.serial.inWaiting()
-        while n > 0:
-            self.serial.read(n)
-            n = self.serial.inWaiting()
-        self.serial.write(b'\r\x01') # ctrl-A: enter raw REPL
-        data = self.serial.read(50)
-        if not data.endswith(b'raw REPL; CTRL-B to exit\r\n>'):
-            self.logger.debug(data)
-            return False
-        return True
-
     def led_toggle(self, led, flash_s=LED_FLASH_TIME_S):
-        self.execbuffer('pyb.LED({}).on()'.format(led))
+        self.pyb.enter_raw_repl()
+        self.pyb.exec('pyb.LED({}).on()'.format(led))
         time.sleep(flash_s)
-        self.execbuffer('pyb.LED({}).off()'.format(led))
+        self.pyb.exec('pyb.LED({}).off()'.format(led))
+        self.pyb.exit_raw_repl()
 
     def scan_ports(self, port=None):
         """ Finds/validates all connected pyboards
@@ -134,37 +119,18 @@ class MicroPyBrd(object):
         self.logger.info("Looking for Pyboard in {}".format(port_candidates))
         for port in port_candidates:
             try:
-                self.serial = serial.Serial(port, baudrate=self.BAUDRATE, timeout=1, interCharTimeout=1)
-                success = self._enter_raw_repl()
-                if not success:
-                    time.sleep(0.1)
-                    self.serial.close()
-                    self.serial = serial.Serial(port, baudrate=self.BAUDRATE, timeout=1, interCharTimeout=1)
-                    success = self._enter_raw_repl()
-
-            except Exception as e:
-                self.logger.info(e)
-                success = False
-
-            finally:
-                self.serial.close()
-
-            if success:
-                self.logger.info("Found micropython on {}, blink RED, then GREEN if ID present...".format(port))
-                # now open proper way and get stored ID...
-                self.open(port)
-
+                self.pyb = pyboard.Pyboard(port)
                 # visual indication we are talking to the pyboard
                 self.led_toggle(self.LED_RED, self.LED_FLASH_TIME_S)
-
                 id = self.get_id()
                 if id is not None:
                     # visual indication success
                     self.led_toggle(self.LED_GREEN, self.LED_FLASH_TIME_S)
-
                 self.pyb.close()
                 ports.append({"port": port, "id": id, "version": VERSION})
-            else:
+
+            except Exception as e:
+                self.logger.info(e)
                 self.logger.info("NO micropython on {}".format(port))
 
         self.logger.info("Found {}".format(ports))
@@ -173,7 +139,7 @@ class MicroPyBrd(object):
     def open(self, port):
         self.logger.debug("open {}".format(port))
         self.pyb = pyboard.Pyboard(port)
-        self.pyb.enter_raw_repl()  # this does a softreset on micropython
+        #self.pyb.enter_raw_repl()  # this does a softreset on micropython
 
     def close(self):
         self.logger.debug("close")
@@ -184,54 +150,14 @@ class MicroPyBrd(object):
         self.logger.info("reset")
         self.pyb.enter_raw_repl()  # this does a softreset on micropython
 
-    def execbuffer(self, buf):
-        """ execute a buffer on the open pyboard
-
-        commands should be formed this way,
-            pyb = MicroPyBrd(logging)
-            pyb.open(<port>)
-            cmds = ['import os', 'print(os.listdir())']
-            cmd = "\n".join(cmds)
-            success, result = pyb.execbuffer(cmd)
-            pyb.close()
-
-        NOTE:  !! to get results back you must wrapt them in a print() !!
-
-        :param buf: string of command(s)
-        :return: success (True/False), result (if any)
-        """
-        # this was copied from pyboard.py
-        try:
-            ret, ret_err = self.pyb.exec_raw(buf + '\n', timeout=1, data_consumer=None)
-        except pyboard.PyboardError as er:
-            self.logger.info(er)
-            self.pyb.close()
-            return False, None
-        except KeyboardInterrupt:
-            return False, None
-        if ret_err:
-            self.pyb.exit_raw_repl()
-            self.pyb.close()
-            pyboard.stdout_write_bytes(ret_err)
-            return False, None
-
-        if ret:
-            return True, ret.decode("utf-8").strip()
-        return True, ""
-
-    def get_files(self):
+    def get_files(self, directory='/flash', long_format=True, recursive=True):
         """ Get the files from an open pyboard
         :return: [ file, ...]
         """
-        files = []
-        cmds = ['import os', 'print(os.listdir())']
-        cmd = "\n".join(cmds)
-        success, result = self.execbuffer(cmd)
-        if success:
-            self.logger.debug("{} {}".format(success, result))
-            # string result => ['main.py', 'pybcdc.inf', 'README.txt', 'ID1', 'boot.py']
-            files = json.loads(result.replace("'", '"'))
-        return files
+        board_files = files.Files(self.pyb)
+        files_list = [f for f in board_files.ls(directory, long_format=long_format, recursive=recursive)]
+        self.logger.debug(files_list)
+        return files_list
 
     def _pyb_del_file(self, file):
         cmds = ['import os', 'os.remove("{}")'.format(file)]
@@ -256,45 +182,53 @@ class MicroPyBrd(object):
         :param id: integer ID of the pyboard
         :return: True on success, False otherwise
         """
-        if self.pyb: self.close()
+        if not self.pyb:
+            # make sure the port is valid
+            port_candidates = serial_ports()
+            if not port in port_candidates:
+                self.logger.error("{} NOT in available ports {}".format(port, port_candidates))
+                return False
 
-        # make sure the port is valid
-        port_candidates = serial_ports()
-        if not port in port_candidates:
-            self.logger.error("{} NOT in available ports {}".format(port, port_candidates))
-            return False
+            # scan the port, make sure its a pyboard
+            current_configs = self.scan_ports(port)
+            valid_port = False
+            for cfg in current_configs:
+                if cfg["port"] == port:
+                    valid_port = True
+                    break
+            if not valid_port:
+                self.logger.error("{} is not a valid micropython port".format(port))
+                return False
 
-        # scan the port, make sure its a pyboard
-        current_configs = self.scan_ports(port)
-        valid_port = False
-        for cfg in current_configs:
-            if cfg["port"] == port:
-                valid_port = True
-                break
-        if not valid_port:
-            self.logger.error("{} is not a valid micropython port".format(port))
-            return False
+            # ok, should be good, open that port
+            self.open(port)
 
-        # ok, should be good, open that port
-        self.open(port)
+        board_files = files.Files(self.pyb)
 
         # remove the old ID if it exists
-        files = self.get_files()
-        for f in files:
-            if f.startswith("ID"):
-                success = self._pyb_del_file(f)
-                if not success:
-                    return False
+        files_list = self.get_files()
+        for f in files_list:
+            if f.startswith("/flash/ID"):
+                # file list format looks like, '/flash/ID1 - 3 bytes'
+                self.logger.info("removing ID: {}".format(f))
+                board_files.rm(f.split(" ")[0])
 
-        # write the new ID
-        cmds = ["f = open('ID{}', 'w')".format(id), 'f.close()']
-        cmd = "\n".join(cmds)
-        success, result = self.execbuffer(cmd)
-        self.logger.info("Writting ID {} success: {}".format(id, success))
+        # create a dummy local file with contents
+        fname = 'ID{}'.format(id)
+        with open("dummy", "w") as f:
+            f.write(fname)
 
-        self.close()
-        self.scan_ports(port)
-        return success
+        try:
+            self.logger.info("Setting ID: {}".format(fname))
+            with open("dummy", "rb") as infile:
+                board_files.put('/flash/{}'.format(fname), infile.read())
+
+            os.remove('dummy')
+            return True
+
+        except Exception as e:
+            self.logger.error(e)
+            return False
 
 
 # Command Line Interface...
@@ -399,10 +333,8 @@ if __name__ == '__main__':
         pyb.open(args.port)
         logging.info("Flashing RED LED of port {}".format(args.port))
         for i in range(10):
-            pyb.execbuffer('pyb.LED(1).on()')
-            time.sleep(0.2)
-            pyb.execbuffer('pyb.LED(1).off()')
-            time.sleep(0.2)
+            pyb.led_toggle(1)
+
         pyb.close()
         did_something = True
 
