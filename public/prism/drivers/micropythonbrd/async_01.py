c4f3b2f9192a02d7f1afe3105986363cf6cf5727
sistemicorp <martin.guthrie@sistemi.ca>
2019-07-20 21:03:34 -0400

    WIP working cool async

--- public/prism/drivers/micropythonbrd/async_01.py
+++ public/prism/drivers/micropythonbrd/async_01.py
@@ -5,16 +5,29 @@ import micropython
 
 
 class MicroPyQueue(object):
+    """ Special Queue for sending commands and getting return items from a MicroPython Process
+
+    """
 
     def __init__(self):
         self.lock = _thread.allocate_lock()
         self.items = []
 
     def put(self, item):
+        """ Put an item into the queue
+
+        :param item: dict of format, {"method": <class_method>, "args": <args>}
+        :return: None
+        """
         with self.lock:
             self.items.append(item)
 
     def get(self, method=None):
+        """ Get an item from the queue
+
+        :param method: set to class_method to return a specific result
+        :return: return item
+        """
         with self.lock:
             if self.items:
                 if method is None:
@@ -27,6 +40,12 @@ class MicroPyQueue(object):
             return None
 
     def peek(self, method=None, all=False):
+        """ Peek at item(s) in the queue, does not remove item
+
+        :param method: if set, returns first item matching method string
+        :param all: returns all items, method param is ignored
+        :return: None for no item, or item
+        """
         with self.lock:
             if all:
                 return self.items
@@ -42,6 +61,11 @@ class MicroPyQueue(object):
             return None
 
     def update(self, item_update):
+        """ Update an item in queue, or append item if it doesn't exist
+
+        :param item_update: new item, of format, {"method": <class_method>, "args": <args>}
+        :return:
+        """
         with self.lock:
             if self.items:
                 for idx, item in iter(self.items):
@@ -53,16 +77,67 @@ class MicroPyQueue(object):
             self.items.append(item)
 
 
-q = MicroPyQueue()
-q_ret = MicroPyQueue()
-
-
 class MicroPyWorker(object):
+    """ Async Worker MicroPython Worker
+    - runs in its own thread
+
+    How to use this:
+        - copy this file over to micropython
+        - import it, import async_01, this will cause it to "run"
+        - send in a command via the q,
+            async_01.w.cmd({"method":"toggle_led", "args":1})
+            async_01.w.cmd({"method":"enable_jig_closed_detect", "args":True})
+        - to get a result,
+            async_01.w.get()
+
+    cmds: Are in this format: {"method": <class_method>, "args": <args>}
+
+    ret: Are in this format: {"method": <class_method>, "value": <value>}
+
+    """
 
     def __init__(self):
-        self.ctx = {'a':0}
+        self.lock = _thread.allocate_lock()
+        self._cmd = MicroPyQueue()
+        self._ret = MicroPyQueue()
+        self.ctx = {'a':0}         # use dict to store static data
         self.timer = pyb.Timer(4)  # create a timer object using timer 4
         self.isr_jig_closed_detect_ref = self.jig_closed_detect
+
+    def cmd(self, cmd):
+        with self.lock:
+            self._cmd.put(cmd)
+
+    def ret(self, method=None):
+        with self.lock:
+            return self._ret.get(method)
+
+    def peek(self, method=None, all=False):
+        with self.lock:
+            return self._ret.peek(method, all)
+
+    def update(self, item_update):
+        with self.lock:
+            return self._ret.update(item_update)
+
+    def run(self):
+        # run on thread
+        while True:
+            item = self._cmd.get()
+            if item is not None:
+                method = item["method"]
+                args = item["args"]
+                method = getattr(self, method, None)
+                if method is not None:
+                    with self.lock:
+                        result = method(args)
+                        print(str(result) + "\n")
+
+        # allows other threads to run, but generally speaking there should be none
+        # timer isr was working before adding this.  But this seems to be the right thing to do.
+        time.sleep(0.01)
+
+    # ===================================================================================
 
     def _toggle_led(self, led, sleep=0.5):
         pyb.LED(led).on()
@@ -74,13 +149,13 @@ class MicroPyWorker(object):
         self._toggle_led(led, sleep)
         self.ctx['a'] += 1
         print('Hello from MicroPyWorker {}'.format(self.ctx['a']))
-        q_ret.put(str({"method": "toggle_led", "value": self.ctx['a']}))
+        self._ret.put(str({"method": "toggle_led", "value": self.ctx['a']}))
         return True
 
     def enable_jig_closed_detect(self, enable=True):
         print("setting jig_closed_detect: {}".format(enable))
         if enable:
-            self.timer.init(freq=2)  # trigger at Hz
+            self.timer.init(freq=1)  # trigger at Hz
             self.timer.callback(self.isr_jig_closed_detect)
         else:
             self.timer.deinit()
@@ -91,39 +166,13 @@ class MicroPyWorker(object):
 
     def jig_closed_detect(self, _):
         self._toggle_led(3, sleep=0.1)
-        q_ret.put(str({"method": "jig_closed_detect", "value": True}))
+        self._ret.put(str({"method": "jig_closed_detect", "value": True}))
         # in reality, if the jig opens, that open result
         # should not be replaces until the PC-side reads that result,
         # so, this code should peek at all messages, if there is a jig open
         # message, then don't post a jig closed message.
 
 
-def testThread():
-    lock = _thread.allocate_lock()
-    w = MicroPyWorker()
-
-    while True:
-        with lock:
-            item = q.get()
-            if item is not None:
-                w_method = item["method"]
-                w_args = item["args"]
-                method = getattr(w,w_method, None)
-                if method is not None:
-                    result = method(w_args)
-                    print(result)
-
-
-_thread.start_new_thread(testThread, ())
-
-# How to use this:
-# - copy this file over to micropython
-# - import it, import async_01, this will cause it to "run"
-# - send in a command via the q,
-#   async_01.q.put({"method":"toggle_led", "args":1})
-#   async_01.q.put({"method":"enable_jig_closed_detect", "args":True})
-# - to get a result,
-#   async_01.q_ret.get()
-
-
+w = MicroPyWorker()
+_thread.start_new_thread(w.run, ())
 
