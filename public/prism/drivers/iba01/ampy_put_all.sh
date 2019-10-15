150e52422b2f38d19a7c6cc6b541bd1b0ff5abc0
sistemicorp <martin.guthrie@sistemi.ca>
2019-10-15 15:26:44 -0400

    update script

--- public/prism/drivers/iba01/ampy_put_all.sh
+++ public/prism/drivers/iba01/ampy_put_all.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
-ampy --port /dev/ttyACM0 put iba01_ads1115.py
-ampy --port /dev/ttyACM0 put iba01_const.py
-ampy --port /dev/ttyACM0 put iba01_INA220.py
-ampy --port /dev/ttyACM0 put iba01_perphs.py
-ampy --port /dev/ttyACM0 put iba01_queue.py
-ampy --port /dev/ttyACM0 put iba01_server.py
-ampy --port /dev/ttyACM0 put iba01_supply12.py
-ampy --port /dev/ttyACM0 put iba01_vbat.py
+
+for f in iba01_*.py
+do
+	echo "Copying file - $f"
+	ampy --port /dev/ttyACM0 put $f
+done
