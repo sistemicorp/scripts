7b74448c7e821d6e933abbffac69c07c235ac63e
sistemicorp <martin.guthrie@sistemi.ca>
2021-03-01 12:13:49 -0500

    initial

--- public/prism/drivers/teensy4/hwdrv_teensy4.py
+++ public/prism/drivers/teensy4/hwdrv_teensy4.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-Sistemi Corporation, copyright, all rights reserved, 2019
+Sistemi Corporation, copyright, all rights reserved, 2021
 Martin Guthrie
 
 """
@@ -9,10 +9,7 @@ import os
 import logging
 import argparse
 from core.sys_log import pub_notice
-
-VERSION = "0.0.1"
-
-NUM_CHANNELS = 1  # set this to simulate multiple channels, range 1-4
+from public.prism.drivers.iba01.list_serial import serial_ports
 
 
 class HWDriver(object):
@@ -25,20 +22,22 @@ class HWDriver(object):
 
       "config": {
         # drivers: list of code to initialize the test environment, must be specified
-        "drivers": ["public.prism.drivers.fake.fake"]
+        "drivers": ["public.prism.drivers.teensy4.hwdrv_teensy4"]
       },
 
-    This is a fake HW driver to simulate and show what a HWDriver is required to do.
     """
     SFN = os.path.basename(__file__)
+    DRIVER_TYPE = "TEENSY4"
+    VERSION = "0.0.1"
 
     def __init__(self, shared_state):
         self.logger = logging.getLogger("SC.{}.{}".format(__class__.__name__, self.SFN))
         self.logger.info("Start")
         self.shared_state = shared_state
+        self._num_chan = 0
 
     def discover_channels(self):
-        """ determine the number of channels, and popultae hw drivers into shared state
+        """ determine the number of channels, and populate hw drivers into shared state
 
         shared_state: a list,
             self.shared_state.add_drivers(DRV_TYPE, [ {}, {}, ... ], shared=True/False)
@@ -57,19 +56,30 @@ class HWDriver(object):
                   0 does not indicate num channels, like a shared hardware driver
                  <0 error
         """
-        drivers = []
-        for i in range(NUM_CHANNELS):
+        sender = "{}.{}".format(self.SFN, __class__.__name__)  # for debug purposes
 
-            # drivers must have an 'id' field, and then whatever...
-            # close field is a method called when channel is torn down
-            drivers.append({'id': i, "version": VERSION, "close": False})
+        port_candidates = serial_ports()
+        self.logger.info("Serial Ports to look for PyBoard {}".format(port_candidates))
 
-            pub_notice("HWDriver:{}: Found channel {}".format(self.SFN, i),
-                       sender="{}.{}".format(self.SFN, __class__.__name__))
+        for port in port_candidates:
+            if "ttyACM" not in port:
+                self.logger.info("skipping port {}...".format(port))
+                continue
 
-        self.shared_state.add_drivers("Fake", drivers)
-        self._num_chan = NUM_CHANNELS
-        self.logger.info("{} channels found".format(self._num_chan))
+            _teensy = { "port": port}
+            self.logger.info("Trying teensy at {}...".format(port))
+
+            # test if this COM port is really a Teensy
+            # create an instance of Teensy()
+
+            # yes, its a Teensy, add it to the list...
+            self.teensys.append(_teensy)
+
+        self._num_chan = len(self.teensys)
+        self.shared_state.add_drivers(self.DRIVER_TYPE, self.pybs, shared=False)
+
+        pub_notice("HWDriver:{}: Found {}!".format(self.SFN, self._num_chan), sender=sender)
+        self.logger.info("Done: {} channels".format(self._num_chan))
         return self._num_chan
 
     def num_channels(self):
@@ -87,6 +97,7 @@ class HWDriver(object):
 
 # -----------------------------------------------------------------------------------------------------
 # CLI interface example to test the driver
+
 
 def parse_args():
     epilog = """
