1bbd518e69ccf748fbcbca7b3c98ea57f74c7a58
sistemicorp <martin.guthrie@sistemi.ca>
2021-12-21 11:35:22 -0500

    remove venv

--- venv/Lib/site-packages/brother_ql/__init__.py
+++
@@ -1,7 +0,0 @@
-
-from .exceptions import *
-
-from .raster import BrotherQLRaster
-
-from .brother_ql_create import create_label
-
--- venv/Lib/site-packages/tubes/test/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- test-case-name: tubes.test -*-
-# Copyright (c) Twisted Matrix Laboratories.
-# See LICENSE for details.
-
-"""
-Test cases for L{tubes}.
-"""
