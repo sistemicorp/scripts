6dcd731a766a8bd20c252f28a107e59bbbf529fe
Martin Guthrie <martin.guthrie@sistemi.ca>
2026-03-06 10:43:50 -0500

    wip seems working

--- public/prism/drivers/brother_ql700/hwdrv_ql700.py
+++ public/prism/drivers/brother_ql700/hwdrv_ql700.py
@@ -1,21 +1,25 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-Sistemi Corporation, copyright, all rights reserved, 2020-2025
+Sistemi Corporation, copyright, all rights reserved, 2020-2026
 Vivian Guthrie, Martin Guthrie
 
 USB Permissions:
-When Prism runs from Docker, it has root permissions so all is good.
+When Prism runs from Docker, it has root permissions, so all is good.
 However, when you are developing, your user account will not have USB permissions.
 Use : sudo chmod -R 777 /dev/bus/usb/
 To unlock USB. This is not a great security solution.
 Also need to: sudo adduser $USER lp
 And then reboot for that to take effect.
 
+It is assumed only one (homogeneous) model of Brother printer is used in your deployment, and
+the driver will only work with that model.
+
 """
 import logging
 from PIL import Image, ImageDraw, ImageFont
 import qrcode
+import re
 import os
 import subprocess
 import base64
@@ -25,11 +29,17 @@ from core.sys_log import pub_notice
 from threading import Lock
 
 VERSION = "0.0.1"
-DRIVER_TYPE = "Brother_QL-700"
+DRIVER_TYPE = "Brother_QL"
+
+# According to PyPi brother_ql profile, these models are supported,
+# QL-500 (✓), QL-550 (✓), QL-560 (✓), QL-570 (✓), QL-580N, QL-650TD, QL-700 (✓),
+# QL-710W (✓), QL-720NW (✓), QL-800 (✓), QL-810W (✓), QL-820NWB (✓), QL-1050 (✓), and QL-1060N (✓)
+# However, only the following has been tested in Prism, add or replace with your specific printer,
+BROTHER_QL_MODELS = ["QL-700"]
 
 
-class BrotherQL700(object):
-    """ Brother Ql-700 Helper Class
+class BrotherQL(object):
+    """ Brother Ql-XXX Helper Class
 
     """
     DRIVER_PATH = "./public/prism/drivers/brother_ql700/"
@@ -159,13 +169,79 @@ class HWDriver(object):
         self.logger.info("Start")
         self._num_chan = 0
 
-    def discover_channels(self):
+    def _attr_str(self, dev, key):
+        raw = dev.attributes.get(key)
+        if raw is None:
+            return ""
+        if isinstance(raw, (bytes, bytearray)):
+            return raw.decode("utf-8", errors="ignore").strip()
+        return str(raw).strip()
+
+    def _usb_port_sort_key(self, usb_dev):
+        """
+        Sort key based on USB topology path.
+        Example usb_dev.sys_name:
+          '1-2'      -> (1, 2)
+          '1-2.3'    -> (1, 2, 3)
+          '2-1.4.2'  -> (2, 1, 4, 2)
+        """
+        sys_name = getattr(usb_dev, "sys_name", "") or ""
+        nums = [int(x) for x in re.findall(r"\d+", sys_name)]
+        if nums:
+            return tuple(nums)
+
+        # fallback: unknown topology goes last, keep deterministic ordering
+        devpath = getattr(usb_dev, "device_path", "") or ""
+        return (10_000, hash(devpath) & 0xFFFF)
+
+    def _find_brother_printers(self, context):
+        """
+        Returns a list of tuples sorted by USB hub port:
+          [(sort_key, devnode, manufacturer, product, usb_sys_name), ...]
+        """
+        candidates = []
+        seen_devnodes = set()
+        model_set = set(BROTHER_QL_MODELS)
+
+        # usblp printer character devices typically show up in usbmisc
+        for lp_dev in context.list_devices(subsystem="usbmisc"):
+            devnode = lp_dev.device_node
+            if not devnode or not devnode.startswith("/dev/usb/lp"):
+                continue
+            if devnode in seen_devnodes:
+                continue
+
+            usb_dev = lp_dev.find_parent(subsystem="usb", device_type="usb_device")
+            if usb_dev is None:
+                continue
+
+            manufacturer = self._attr_str(usb_dev, "manufacturer")
+            product = self._attr_str(usb_dev, "product")
+
+            if "Brother" not in manufacturer:
+                continue
+            if product not in model_set:
+                continue
+
+            sort_key = self._usb_port_sort_key(usb_dev)
+            usb_sys_name = getattr(usb_dev, "sys_name", "")
+            candidates.append((sort_key, devnode, manufacturer, product, usb_sys_name))
+            seen_devnodes.add(devnode)
+
+        candidates.sort(key=lambda x: x[0])
+        return candidates
+
+    def discover_channels(self, scriptArgs=None):
         """ Determine the number of channels, and populate hw drivers into shared state
 
-        This driver is for the Brother QL-700 only.
-        Multiple printers are not supported by this example driver.
-        Multiple printers could be supported by assigning the /dev/usb/lp# to the channel
-        number.
+        scriptArgs = {"only_one": true|false, "test_label": true|false}
+
+            only_one = Only one Printed is expected to be found and will be shared
+            test_label = Print a test label before starting the test script
+
+        This driver is for the Brother QL only.
+        Multiple printers will be ordered by the USB hub port they are connected to. Presumably,
+        each printer is associated with a Jig, so the order of the printers is important.
 
         [ {"id": i,                    # ~slot number of the channel (see Note 1)
            "version": <VERSION>,       # version of the driver
@@ -198,38 +274,17 @@ class HWDriver(object):
         id = 0
         context = pyudev.Context()
 
-        for device in context.list_devices():
-            if device.attributes.get("manufacturer", False) and "Brother" in device.attributes.get("manufacturer").decode('utf-8'):
-                if "QL-700" in str(device.attributes.get("product")):
-
-                    if False: # run snippet to show all kinds of useful stuff
-                        for a in device.attributes.available_attributes:
-                            self.logger.info(f"{a} - {str(device.attributes.get(a))}")
-                        friendly_name = None
-                        if 'ID_MODEL' in device:
-                            friendly_name = device['ID_MODEL']
-                        elif 'ID_MODEL_ENC' in device:
-                            friendly_name = device['ID_MODEL_ENC']
-                        elif 'ID_VENDOR' in device:
-                            friendly_name = device['ID_VENDOR']
-                        elif 'ID_USB_PRODUCT' in device:
-                            friendly_name = device['ID_USB_PRODUCT']
-                        elif 'ID_FS_LABEL' in device:  # For file systems/storage devices
-                            friendly_name = device['ID_FS_LABEL']
-                        self.logger.info(f"friendly_name: {friendly_name}")
-
-                    # TODO: find the path programmatically
-                    # TODO: support multiple printers
-                    p = '/dev/usb/lp0'
-
-                    drivers.append({"id": id,
-                                    "version": VERSION,
-                                    "hwdrv": BrotherQL700(id, p),
-                                    "play": None,
-                                    "show_pass_fail": None,
-                                    "show_msg": None,
-                                    "close": None})
-                    id += 1
+        printers = self._find_brother_printers(context)
+        for _, devnode, manufacturer, product, usb_sys_name in printers:
+            self.logger.info("Found printer: %s %s at %s (usb=%s)", manufacturer, product, devnode, usb_sys_name)
+            drivers.append({"id": id,
+                            "version": VERSION,
+                            "hwdrv": BrotherQL(id, devnode),
+                            "play": None,
+                            "show_pass_fail": None,
+                            "show_msg": None,
+                            "close": None})
+            id += 1
 
         if not drivers:
             self.logger.error("printer not found")
@@ -237,23 +292,24 @@ class HWDriver(object):
             return -1, DRIVER_TYPE, []
 
         # do not allow the test script validation step to succeed if can't print a test label
-        # TODO: make printing a test label a parameter from the script config
-        for d in drivers:
-            id, path = d["hwdrv"].get_id_path()
-            success = d["hwdrv"].print_ruid_barcode("id{}-{}".format(id, path))
-            if not success:
-                self.logger.error("failed to print")
-                pub_notice("HWDriver:{}: failed to print".format(self.SFN), sender="discover_channels", type=PUB.NOTICES_ERROR)
-                return -1, DRIVER_TYPE, []
+        if scriptArgs and scriptArgs.get("test_label", False):
+            for d in drivers:
+                id, path = d["hwdrv"].get_id_path()
+                success = d["hwdrv"].print_ruid_barcode("id{}-{}".format(id, path))
+                if not success:
+                    self.logger.error("failed to print")
+                    pub_notice("HWDriver:{}: failed to print".format(self.SFN), sender="discover_channels", type=PUB.NOTICES_ERROR)
+                    return -1, DRIVER_TYPE, []
 
-            #pub_notice("HWDriver:{}: found {} {}".format(self.SFN, id, path), sender="discover_channels", type=PUB.NOTICES_NORMAL)
+        pub_notice("HWDriver:{}: found {}".format(self.SFN, id),
+                   sender="discover_channels",
+                   type=PUB.NOTICES_NORMAL)
 
         # Setting number of channels to zero means this device is shared across all test fixtures
-        # TODO: change when support for printer per test jig is required/used
-        self._num_chan = 0
+        if scriptArgs and scriptArgs.get('only_one', False):
+            self._num_chan = 0
 
-        # by returning 0, it means this return values DOES not represent number of channels
-        return 0, DRIVER_TYPE, drivers
+        return self._num_chan, DRIVER_TYPE, drivers
 
     def num_channels(self):
         return self._num_chan
@@ -263,7 +319,7 @@ class HWDriver(object):
 
 # ===============================================================================================
 # Debugging code
-# - Test your hardware discover here by running this file from PyCharm (be sure to set the working
+# - Test your hardware discovery here by running this file from PyCharm (be sure to set the working
 #   directory as ~/git/scripts, else imports will fail)
 # - the purpose is to valid discover_channels() is working
 # - you will have to comment out pub_notice() and the core import whilst using this
@@ -274,5 +330,6 @@ if __name__ == '__main__':
     logger.setLevel(logging.INFO)
 
     d = HWDriver()
-    d.discover_channels()
+    #d.discover_channels()
+    d.discover_channels({"test_label": True})
     logger.info("Number channels: {}".format(d.num_channels()))
